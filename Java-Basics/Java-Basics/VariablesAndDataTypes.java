c44abe5d12246bd79ffab3c933ebd686eb16337e
Diwakar Sharma <diwakarsharma789456@gmail.com>
2026-09-20 11:31:23 +0530

    Add Java variables and data types example
    Added a Java example demonstrating common variables and primitive data types.

---
+++ Java-Basics/Java-Basics/VariablesAndDataTypes.java
@@ -0,0 +1,17 @@
+public class VariablesAndDataTypes {
+
+    public static void main(String[] args) {
+
+        String name = "Diwakar";
+        int age = 22;
+        double percentage = 75.5;
+        boolean cybersecurityStudent = true;
+        char grade = 'A';
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Cybersecurity Student: " + cybersecurityStudent);
+        System.out.println("Grade: " + grade);
+    }
+}
