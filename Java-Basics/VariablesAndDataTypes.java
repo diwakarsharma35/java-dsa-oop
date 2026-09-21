465e10566d286f9ee5692ac3449aa634dfcd970d
Diwakar Sharma <diwakarsharma789456@gmail.com>
2026-09-21 11:08:56 +0530

    Add Java variables and data types example

---
+++ Java-Basics/VariablesAndDataTypes.java
@@ -0,0 +1,16 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        int age = 22;
+        double percentage = 85.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Diwakar";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
