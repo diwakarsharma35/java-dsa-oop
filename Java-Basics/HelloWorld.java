9a2c96bdee00fc417a287c36dfb8b0985eff3146
Diwakar Sharma <diwakarsharma789456@gmail.com>
2026-09-21 11:07:32 +0530

    Add Hello World Java program

---
+++ Java-Basics/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+        System.out.println("Learning Java, DSA & OOP.");
+    }
+}
